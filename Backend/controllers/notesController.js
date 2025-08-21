47a332243de3fd4ce6b6e2f617abbc6f1af0e2d4
RenanMarinho809 <renanmarinho809@gmail.com>
2025-08-21 16:19:10 -0300

    Backend of the project made

---
+++ Backend/controllers/notesController.js
@@ -0,0 +1,59 @@
+import Note from "../models/Notes.js";
+
+export const getAllNotes = async (req, res) => {
+    try{
+        const notes = await Note.find();
+        res.status(200).json(notes);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({message: 'Error getting notes'});
+    }
+};
+
+export const createNote = async (req, res) => {
+    try {
+        const {title, content} = req.body;
+
+        const newNote = await Note.create({
+            title,
+            content
+        });
+        res.status(201).json(newNote);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({message: 'Error creating note'});
+    }
+}
+
+export const updateNote = async (req, res) => {
+    try {
+        const { title, content } = req.body;
+        const updatedNote = await Note.findByIdAndUpdate(
+            req.params.id,
+            { title, content },
+            {
+                new: true,
+            }
+        );
+
+        if (!updatedNote) return res.status(404).json({ message: "Note not found" });
+
+        res.status(200).json(updatedNote);
+    } catch (error) {
+        console.error("Error in updateNote controller", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const deleteNote = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+       const deleteNote = await Note.findByIdAndDelete(id);
+       if (!deleteNote) return res.status(404).json({ message: "Note not found" });
+       res.status(200).json({message: 'Note deleted'});
+    }catch(err){
+        console.log(err);
+        res.status(500).json({message: 'Error deleting note'});
+    }
+};
---
+++ Backend/src/server.js
@@ -0,0 +1,19 @@
+import express from "express"
+import router from "../routes/notesRoute.js";
+import {connectDB} from "../config/db.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+console.log(process.env.MONGO_URI);
+
+const port = process.env.PORT || 5001;
+
+const app = express()
+app.use(express.json())
+app.use("/api/notes",router)
+
+connectDB()
+
+app.listen(port, () => {
+    console.log("Server running on port 5001");
+});
