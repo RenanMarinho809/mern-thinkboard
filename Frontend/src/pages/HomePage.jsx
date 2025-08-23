9e7fe81850521cd3e4b0ec1a6fbdf7616fb88260
RenanMarinho809 <renanmarinho809@gmail.com>
2025-08-23 14:27:40 -0300

    Done the Web app think board

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,27 +1,30 @@
-import React, {useEffect, useState} from 'react'
-import Navbar from "../components/Navbar.jsx";
-import RateLimitedUi from "../components/RateLimitedUI.jsx";
-import axios from "axios";
+import { useState } from "react";
+import Navbar from "../components/Navbar";
+import RateLimitedUI from "../components/RateLimitedUI";
+import { useEffect } from "react";
+import api from "../lib/axios";
 import toast from "react-hot-toast";
-import NoteCard from "../components/NoteCard.jsx";
+import NoteCard from "../components/NoteCard";
+import NotesNotFound from "../components/NotesNotFound.jsx";
+
 
 const HomePage = () => {
-    const [isRatedLimited , setIsRatedLimited] = useState(true);
+    const [isRateLimited, setIsRateLimited] = useState(false);
     const [notes, setNotes] = useState([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchNotes = async () => {
             try {
-                const res = await axios.get("http://localhost:5001/api/notes")
+                const res = await api.get("/notes");
                 console.log(res.data);
                 setNotes(res.data);
-                setIsRatedLimited(false);
+                setIsRateLimited(false);
             } catch (error) {
                 console.log("Error fetching notes");
                 console.log(error.response);
                 if (error.response?.status === 429) {
-                    setIsRatedLimited(true);
+                    setIsRateLimited(true);
                 } else {
                     toast.error("Failed to load notes");
                 }
@@ -36,22 +39,23 @@ const HomePage = () => {
     return (
         <div className="min-h-screen">
             <Navbar />
-            {isRatedLimited && <RateLimitedUi />}
+
+            {isRateLimited && <RateLimitedUI />}
 
             <div className="max-w-7xl mx-auto p-4 mt-6">
                 {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
 
-                {notes.length > 0 && !isRatedLimited && (
+                {notes.length === 0 && !isRateLimited && <NotesNotFound />}
+
+                {notes.length > 0 && !isRateLimited && (
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                         {notes.map((note) => (
-                            <div>
-                                <NoteCard  key={note._id} note={note} />
-                            </div>
+                            <NoteCard key={note._id} note={note} setNotes={setNotes} />
                         ))}
                     </div>
                 )}
             </div>
         </div>
-    )
-}
-export default HomePage
+    );
+};
+export default HomePage;
